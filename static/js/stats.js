dff0996f1ec3dcf93a95c9776fdfa9fcee92fb9f
John McLear <john@mclear.co.uk>
2021-01-08 02:02:35 +0000

    autofixes from Etherpad checkPlugins.js

--- static/js/stats.js
+++ static/js/stats.js
@@ -3,33 +3,33 @@ if (typeof exports === 'undefined') {
 }
 
 exports.stats = {
-  init(){
+  init() {
     stats.update();
   },
-  show(){
+  show() {
     $('#stats').show();
-    $('#stats').css("top", $('#editorcontainer').offset().top+'px');
-//    $('#options-stickychat').attr("checked","checked");
+    $('#stats').css('top', `${$('#editorcontainer').offset().top}px`);
+    //    $('#options-stickychat').attr("checked","checked");
     exports.stats.update();
   },
-  hide(){
+  hide() {
     $('#stats').hide();
   },
-  update(){
-    var html = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").html();
-    var text = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").text();
-    $('#length > .stats').html( text.replace(/\s/g,"").length );
-    $('#lengthWhitespace > .stats').html( text.length );
-    $('#wordCount > .stats').html( exports.wordCount());
-    $('#revCount > .stats').html( pad.getCollabRevisionNumber() );
-    $('#savedRevCount > .stats').html( clientVars.savedRevisions.length ); // TODO cake doesnt update in real time
-    $('#authorCount > .stats').html( Object.keys(clientVars.collab_client_vars.historicalAuthorData).length );
-    $('#wordsContributed > .stats').html( tb(exports.stats.authors.numberOfWords()) );
-    $('#linesContributed > .stats').html( tb(exports.stats.authors.numberOfLines()) );
-    $('#linesAsOnlyContributor > .stats').html( tb(exports.stats.authors.numberOfLinesExclusive()) );
-    $('#numberOfCharsIncWS > .stats').html( tb(exports.stats.authors.numberOfChars()) );
-    $('#numberOfCharsExcWS > .stats').html( tb(exports.stats.authors.numberOfCharsExcWS()) );
-  }
+  update() {
+    const html = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find('#innerdocbody').html();
+    const text = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find('#innerdocbody').text();
+    $('#length > .stats').html(text.replace(/\s/g, '').length);
+    $('#lengthWhitespace > .stats').html(text.length);
+    $('#wordCount > .stats').html(exports.wordCount());
+    $('#revCount > .stats').html(pad.getCollabRevisionNumber());
+    $('#savedRevCount > .stats').html(clientVars.savedRevisions.length); // TODO cake doesnt update in real time
+    $('#authorCount > .stats').html(Object.keys(clientVars.collab_client_vars.historicalAuthorData).length);
+    $('#wordsContributed > .stats').html(tb(exports.stats.authors.numberOfWords()));
+    $('#linesContributed > .stats').html(tb(exports.stats.authors.numberOfLines()));
+    $('#linesAsOnlyContributor > .stats').html(tb(exports.stats.authors.numberOfLinesExclusive()));
+    $('#numberOfCharsIncWS > .stats').html(tb(exports.stats.authors.numberOfChars()));
+    $('#numberOfCharsExcWS > .stats').html(tb(exports.stats.authors.numberOfCharsExcWS()));
+  },
 };
 
 
@@ -38,7 +38,7 @@ exports.wordCount = function () {
   $('iframe[name="ace_outer"]').contents().find('iframe').contents().find('#innerdocbody').contents().each(function () {
     let lineCount = 0;
     $(this).contents().each(function () {
-      var numberOf = $(this).text().split(' ');
+      let numberOf = $(this).text().split(' ');
       numberOf = arrayDelete(numberOf, ''); // dont include spaces or line breaks or other nastyness
       lineCount += numberOf.length;
     });
@@ -48,24 +48,24 @@ exports.wordCount = function () {
 };
 
 exports.stats.authors = {
-  numberOfWords(){
-    var results = {};
+  numberOfWords() {
+    const results = {};
     // go through each word, does it have the class of this author?
     // output format.  John -- 6, Dave -- 9
-    $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").contents().each(function(){
-      $(this).contents().each(function(){
-        var line = this;
-        var classes = $(this).attr("class");
-        if(classes){
-          classes = classes.split(" ");
-          $.each(classes, function(k, spanClass){
-            if( spanClass.indexOf("author") !== -1){ // if an author class exists on this span
+    $('iframe[name="ace_outer"]').contents().find('iframe').contents().find('#innerdocbody').contents().each(function () {
+      $(this).contents().each(function () {
+        const line = this;
+        let classes = $(this).attr('class');
+        if (classes) {
+          classes = classes.split(' ');
+          $.each(classes, (k, spanClass) => {
+            if (spanClass.indexOf('author') !== -1) { // if an author class exists on this span
               // how many words are in this string?
-              var number = $(line).text().split(" ").length;
-              if( !results[ spanClass ] ){
-                results[ spanClass ] = number;
-              }else{
-                results[ spanClass ] = results[ spanClass ] + number;
+              const number = $(line).text().split(' ').length;
+              if (!results[spanClass]) {
+                results[spanClass] = number;
+              } else {
+                results[spanClass] = results[spanClass] + number;
               }
             }
           });
@@ -74,112 +74,107 @@ exports.stats.authors = {
     });
     return results;
   },
-  numberOfLines(){
-    var results = {};
+  numberOfLines() {
+    const results = {};
     // output format.  John -- 2, Dave -- 3
-    $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").contents().each(function(){ // each line
-      $(this).contents().each(function(){
-        var line = this;
-        var classes = $(this).attr("class");
-        if(classes){
-          classes = classes.split(" ");
-          $.each(classes, function(k, spanClass){
-            if( spanClass.indexOf("author") !== -1){ // if an author class exists on this span
+    $('iframe[name="ace_outer"]').contents().find('iframe').contents().find('#innerdocbody').contents().each(function () { // each line
+      $(this).contents().each(function () {
+        const line = this;
+        let classes = $(this).attr('class');
+        if (classes) {
+          classes = classes.split(' ');
+          $.each(classes, (k, spanClass) => {
+            if (spanClass.indexOf('author') !== -1) { // if an author class exists on this span
               // how many words are in this string?
-              var number = $(line).text().split(" ").length;
-              if( !results[ spanClass ] ){
-                results[ spanClass ] = 1;
-              }else{
-                results[ spanClass ] = results[ spanClass ] + 1;
+              const number = $(line).text().split(' ').length;
+              if (!results[spanClass]) {
+                results[spanClass] = 1;
+              } else {
+                results[spanClass] = results[spanClass] + 1;
               }
             }
           });
         }
       });
-
     });
     return results;
-
   },
-  numberOfLinesExclusive(){
-    var results = {};
-    var lineCount = 1;
+  numberOfLinesExclusive() {
+    const results = {};
+    let lineCount = 1;
     // output format.  John -- 1, Dave -- 1
-    $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").contents().each(function(){ // For Each LINE
-      var line = {};
-      $(this).contents().each(function(){ // For SPAN!
-        var classes = $(this).attr("class");
-        if(classes){
-          classes = classes.split(" ");
-          $.each(classes, function(k, spanClass){
-            if( spanClass.indexOf("author") !== -1){ // if an author class exists on this span
-              if( !line[ lineCount ] ){
-                line[ lineCount ] = {};
-                line[ lineCount ].author = spanClass; // first author!
-              }else{
-                delete line[ lineCount ];// already has an author so nuke!
+    $('iframe[name="ace_outer"]').contents().find('iframe').contents().find('#innerdocbody').contents().each(function () { // For Each LINE
+      const line = {};
+      $(this).contents().each(function () { // For SPAN!
+        let classes = $(this).attr('class');
+        if (classes) {
+          classes = classes.split(' ');
+          $.each(classes, (k, spanClass) => {
+            if (spanClass.indexOf('author') !== -1) { // if an author class exists on this span
+              if (!line[lineCount]) {
+                line[lineCount] = {};
+                line[lineCount].author = spanClass; // first author!
+              } else {
+                delete line[lineCount];// already has an author so nuke!
               }
             }
           });
-
-
         }
         // End Span
       });
-      var lineHasOneAuthor = (line[lineCount])
-      if(lineHasOneAuthor){
+      const lineHasOneAuthor = (line[lineCount]);
+      if (lineHasOneAuthor) {
         // add author to results obj
-        var author = line[lineCount].author;
-        results[author] = (results[author] +1) || 1
+        const author = line[lineCount].author;
+        results[author] = (results[author] + 1) || 1;
       }
       lineCount++;
       // End Line
     });
     return results;
   },
-  numberOfChars(){
-    var results = {};
+  numberOfChars() {
+    const results = {};
     // output format.  John -- 6, Dave -- 9
-    $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").contents().each(function(){
-      $(this).contents().each(function(){
-        var classes = $(this).attr("class");
-        if(classes){
-          classes = classes.split(" ");
-          var number = $(this).text().length;
-          $.each(classes, function(k, spanClass){
-            if( spanClass.indexOf("author") !== -1){ // if an author class exists on this span
-              results[ spanClass ] = number;
-            }else{
-              results[ spanClass ] = results [ spanClass] + 1;
-            }
-          });
-        };
-      });
-    });
-    return results;
-  },
-  numberOfCharsExcWS(){
-    var results = {};
-    // output format.  John -- 6, Dave -- 9
-    $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").contents().each(function(){ 
-      $(this).contents().each(function(){
-        var classes = $(this).attr("class");
-        if(classes){
- 
-          classes = classes.split(" ");
-          var number = $(this).text().replace(/\s/g,"").length; // get length without whitespace
-          $.each(classes, function(k, spanClass){
-            if( classes.indexOf("author") !== -1){ // if an author class exists on this span
-              results[ spanClass ] = number;
-            }else{
-              results[ spanClass ] = results[ spanClass] + number;
+    $('iframe[name="ace_outer"]').contents().find('iframe').contents().find('#innerdocbody').contents().each(function () {
+      $(this).contents().each(function () {
+        let classes = $(this).attr('class');
+        if (classes) {
+          classes = classes.split(' ');
+          const number = $(this).text().length;
+          $.each(classes, (k, spanClass) => {
+            if (spanClass.indexOf('author') !== -1) { // if an author class exists on this span
+              results[spanClass] = number;
+            } else {
+              results[spanClass] = results[spanClass] + 1;
             }
           });
         }
       });
     });
     return results;
-  }
+  },
+  numberOfCharsExcWS() {
+    const results = {};
+    // output format.  John -- 6, Dave -- 9
+    $('iframe[name="ace_outer"]').contents().find('iframe').contents().find('#innerdocbody').contents().each(function () {
+      $(this).contents().each(function () {
+        let classes = $(this).attr('class');
+        if (classes) {
+          classes = classes.split(' ');
+          const number = $(this).text().replace(/\s/g, '').length; // get length without whitespace
+          $.each(classes, (k, spanClass) => {
+            if (classes.indexOf('author') !== -1) { // if an author class exists on this span
+              results[spanClass] = number;
+            } else {
+              results[spanClass] = results[spanClass] + number;
+            }
+          });
+        }
+      });
+    });
+    return results;
+  },
 };
 
 exports.postAceInit = function (hook, context) {
@@ -205,8 +200,7 @@ exports.aceEditEvent = function (hook_name, event, cb) {
 
 exports.className2Author = function (className) {
   return className.substring(7).replace(/[a-y0-9]+|-|z.+?z/g, (cc) => {
-    if (cc == '-') {return '.';}
-    else if (cc.charAt(0) == 'z') {
+    if (cc == '-') { return '.'; } else if (cc.charAt(0) == 'z') {
       return String.fromCharCode(Number(cc.slice(1, -1)));
     } else {
       return cc;
@@ -215,15 +209,15 @@ exports.className2Author = function (className) {
 };
 
 exports.getAuthorClassName = function (author) {
-  return 'ep_cursortrace-' + author.replace(/[^a-y0-9]/g, (c) => {
+  return `ep_cursortrace-${author.replace(/[^a-y0-9]/g, (c) => {
     if (c == '.') return '-';
-    return `z${  c.charCodeAt(0)  }z`;
-  });
+    return `z${c.charCodeAt(0)}z`;
+  })}`;
 };
 
 function tb(data) { // turns data object into a table
   let table = '<table>';
-  for (let value in data) {
+  for (const value in data) {
     table += `<tr><td class='statsAuthorColor' style='background-color:${authorColorFromClass(value)}'></td><td>${authorNameFromClass(value)}:</td><td>${data[value]}</td></tr>`;
   }
   table += '</table>';
@@ -232,10 +226,10 @@ function tb(data) { // turns data object into a table
 
 function authorNameFromClass(authorClass) { // turn authorClass into authorID then authorname..
   // get the authorID from the class..
-  let authorId = exports.className2Author(authorClass);
+  const authorId = exports.className2Author(authorClass);
 
   // It could always be me..
-  let myAuthorId = pad.myUserInfo.userId;
+  const myAuthorId = pad.myUserInfo.userId;
   if (myAuthorId == authorId) {
     return 'Me';
   }
@@ -252,16 +246,16 @@ function authorNameFromClass(authorClass) { // turn authorClass into authorID th
   if (name) return name;
 
   // Else go historical
-  let historical = clientVars.collab_client_vars.historicalAuthorData[authorId];
+  const historical = clientVars.collab_client_vars.historicalAuthorData[authorId];
   return (historical && historical.name) || 'Unknown Author';
 }
 
 function authorColorFromClass(authorClass) { // turn authorClass into authorID then authorname..
   // get the authorID from the class..
-  let authorId = exports.className2Author(authorClass);
+  const authorId = exports.className2Author(authorClass);
 
   // It could always be me..
-  let myAuthorId = pad.myUserInfo.userId;
+  const myAuthorId = pad.myUserInfo.userId;
   if (myAuthorId == authorId) {
     return '#fff';
   }
@@ -278,7 +272,7 @@ function authorColorFromClass(authorClass) { // turn authorClass into authorID t
   if (color) return color;
 
   // Else go historical
-  let historical = clientVars.collab_client_vars.historicalAuthorData[authorId];
+  const historical = clientVars.collab_client_vars.historicalAuthorData[authorId];
   return (historical && historical.color) || '#fff';
 }
 
